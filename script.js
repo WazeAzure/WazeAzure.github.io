934cedc5cc364526f40a8228c27e80d0b3fbea67
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2020-08-03 15:19:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 $(function(){
 
   $('.modal-out').hide();
-
   $('#modal').click(
     function(){
       $('body').find('.modal-out').show();
     }
   )
-
   $('#close').click(function(){
     $('.modal-out').hide();
   })
+
+  $('.icon').hover(
+    function(){
+      $(this).css('opacity','0.7');
+    },
+    function(){
+      $(this).css('opacity','1');
+    }
+  )
 });
